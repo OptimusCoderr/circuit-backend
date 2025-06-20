7c483f644dfa7a80fdc055f15ff95644cec85f68
Anulunko Chukwuebuka Oliver <chukwuebukaanulunko@gmail.com>
2025-06-20 15:40:13 +0100

    Update Control.model.js

--- src/models/Control.model.js
+++ src/models/Control.model.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const controlSchema = new mongoose.Schema({
-
   circuitstate: {
+    type: String,
+  },
+  circuitstate1: {
     type: String,
   },
 }, {timestamps: true}
